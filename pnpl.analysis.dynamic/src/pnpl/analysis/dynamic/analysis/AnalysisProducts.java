42de025cc27d49eed90d02c1e7c3509d16212359
Javier Pastor <javpasto@ucm.es>
2024-05-15 22:57:32 +0200

    Add files via upload
    Last commit was an error.

---
+++ pnpl.analysis.dynamic/src/pnpl/analysis/dynamic/analysis/AnalysisProducts.java
@@ -0,0 +1,18 @@
+package pnpl.analysis.dynamic.analysis;
+
+
+public class AnalysisProducts extends AbstractAnalysisDynamic {
+
+	protected boolean runProductAnalysis() {
+		//if (pn == null) return false;
+
+		// Nota para Javier de EG: Aqui va el codigo en el caso de un producto de la PNPL
+		return true;
+	}
+
+	@Override
+	public boolean run() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
---
+++ pnpl.analysis.dynamic/src/pnpl/analysis/dynamic/handlers/ProductsHandler.java
@@ -0,0 +1,10 @@
+package pnpl.analysis.dynamic.handlers;
+
+public class ProductsHandler extends AbstractAnalysisHandler {
+
+	@Override
+	protected String getExtensionPointIdentifier() {
+		return "pnpl.analysis.dynamic.analysis.products";
+	}
+
+}
